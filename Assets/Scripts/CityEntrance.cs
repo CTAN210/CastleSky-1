eea4429d7bd3af572a644a11c29539ce60a60a9e
minangjn <74585835+minangjn@users.noreply.github.com>
2021-10-19 14:42:09 +0800

    add buttons to cities

--- Assets/Scripts/CityEntrance.cs
+++ Assets/Scripts/CityEntrance.cs
@@ -3,44 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement; 
+using UnityEngine.EventSystems;
 
-public class CityEntrance : MonoBehaviour
+public class CityEntrance : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private GameObject entrancePopup;
-    public GameObject player;
+    //[SerializeField] private GameObject entrancePopup;
+    //public GameObject player;
     public bool playerInRange;
     string buildingName;
     string sceneToLoad;
+    Button btn;
+    GameObject city;
 
     void Start()
     {
-        player = GameObject.Find("Player");
         playerInRange = false;
-        entrancePopup.SetActive(false);
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && playerInRange == true){
-            if (buildingName == "Fractions_1"){
-                sceneToLoad = "Math World Scene";
-            }
-            else if (buildingName == "Fractions_2"){
-                sceneToLoad = "Science World Scene";
-            }
-            SceneManager.LoadScene(sceneToLoad);
-        }
+
     }
+
+	void TaskOnClick(){
+        sceneToLoad = "Science World Scene";
+        switch(buildingName){
+
+            case "Fractions_1":
+                sceneToLoad = "Math World Scene";
+                break;
+
+            case "Fractions_2":
+                //sceneToLoad = "Math World Scene";
+                break;
+
+            case "Fractions_3":
+                //sceneToLoad = "Math World Scene";
+                break;
+
+            case "Fractions_4":
+                //sceneToLoad = "Math World Scene";
+                break;
+
+            case "Fractions_5":
+                //sceneToLoad = "Math World Scene";
+                break;
+
+            case "Fractions_6":
+                sceneToLoad = "Math World Scene";
+                break;
+
+            case "Fractions_7":
+                //sceneToLoad = "Math World Scene";
+                break;
+
+            case "Fractions_8":
+                //sceneToLoad = "Math World Scene";
+                break;
+
+            case "Fractions_9":
+                //sceneToLoad = "Math World Scene";
+                break;
+
+            case "Fractions_10":
+                //sceneToLoad = "Math World Scene";
+                break;
+        }
+        SceneManager.LoadScene(sceneToLoad);
+	}
 
     void OnTriggerEnter2D (Collider2D other)
     {
         Debug.Log("Player has entered entrance");
         if (other.CompareTag("Player") && !other.isTrigger){
             playerInRange = true;
-            entrancePopup.SetActive(true);
-            entrancePopup.transform.position = player.transform.position + new Vector3(2,2,0);
             buildingName = this.gameObject.name;
-            entrancePopup.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = buildingName;
+            //entrancePopup.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = buildingName;
         }
     }
 
@@ -48,8 +86,26 @@ public class CityEntrance : MonoBehaviour
     {
         Debug.Log("Player has left entrance");
         if (other.CompareTag("Player") && !other.isTrigger){
-            entrancePopup.SetActive(false);
             playerInRange = false;
+        }
+    }
+
+    //Detect if the Cursor starts to pass over the GameObject
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        if (playerInRange){
+            city = GameObject.Find(name);
+            city.GetComponent<Image>().color = new Color(192/255f ,192/255f, 192/255f);
+            btn = city.GetComponentInChildren<Button>();
+            btn.onClick.AddListener(TaskOnClick);
+        }
+    }
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        if (playerInRange){
+            city.GetComponent<Image>().color = new Color(1, 1, 1);
         }
     }
 }
